20bb2b53869530b010471d52c3fa55fabfba75df
Ángel Francisco García Guzmán <angelgarciag14@gmail.com>
2024-11-26 11:20:30 -0600

    Fix: Funcionalidad de camara para jugador

--- FireRescue/Assets/Scripts/Controls/CameraHorizontalMovement.cs
+++ FireRescue/Assets/Scripts/Controls/CameraHorizontalMovement.cs
@@ -4,6 +4,15 @@ public class CameraHorizontalMovement : MonoBehaviour
 {
     public float moveSpeed = 5f; // Velocidad de movimiento
 
+    void Start()
+    {
+        // Posición inicial
+        transform.position = new Vector3(4.55f, 4.72f, -2.32f);
+
+        // Rotación inicial
+        transform.rotation = Quaternion.Euler(46.659f, 0f, 0f);
+    }
+
     void Update()
     {
         // Obtener entrada del jugador
@@ -15,5 +24,11 @@ public class CameraHorizontalMovement : MonoBehaviour
 
         // Mover la cámara
         transform.Translate(movement, Space.World);
+
+        // Limitar posición de la cámara
+        Vector3 clampedPosition = transform.position;
+        clampedPosition.x = Mathf.Clamp(clampedPosition.x, -2f, 12f); // Limitar X
+        clampedPosition.z = Mathf.Clamp(clampedPosition.z, -5f, 5f); // Limitar Z
+        transform.position = clampedPosition;
     }
 }
